aefba06d4ea45c940b178f1e0ccf0a1e2c5ec863
Akash <samalaakash4@gmail.com>
2026-03-16 16:14:19 +0530

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <h1>React E-commerce Frontend Application</h1>
+      <p>Welcome to the e-commerce project</p>
+    </div>
+  );
+}
+
+export default App;
